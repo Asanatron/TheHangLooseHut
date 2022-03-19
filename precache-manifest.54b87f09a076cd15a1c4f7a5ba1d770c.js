e1cf90866ce46083fff3c7330707f116e8b418ea
vaibhavsethia <vaibhavsethia1998@gmail.com>
2022-03-19 13:23:49 +0530

    Updates

---
+++ precache-manifest.54b87f09a076cd15a1c4f7a5ba1d770c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "1ca6458f0ccf8dee3799",
+    "url": "/TheHangLooseHut/static/css/main.7fbde97b.chunk.css"
+  },
+  {
+    "revision": "1ca6458f0ccf8dee3799",
+    "url": "/TheHangLooseHut/static/js/main.1ca6458f.chunk.js"
+  },
+  {
+    "revision": "69f2a6f844f6e07a36f4",
+    "url": "/TheHangLooseHut/static/js/1.69f2a6f8.chunk.js"
+  },
+  {
+    "revision": "874b301bf52a7b160cac",
+    "url": "/TheHangLooseHut/static/css/2.2e40aa84.chunk.css"
+  },
+  {
+    "revision": "874b301bf52a7b160cac",
+    "url": "/TheHangLooseHut/static/js/2.874b301b.chunk.js"
+  },
+  {
+    "revision": "b587c54bf4e0da748170",
+    "url": "/TheHangLooseHut/static/js/runtime~main.b587c54b.js"
+  },
+  {
+    "revision": "7839023b6ded8b603f48de825851e755",
+    "url": "/TheHangLooseHut/static/media/Logo.7839023b.png"
+  },
+  {
+    "revision": "a14783af281645cbfca6487b9fd52ada",
+    "url": "/TheHangLooseHut/index.html"
+  }
+];
