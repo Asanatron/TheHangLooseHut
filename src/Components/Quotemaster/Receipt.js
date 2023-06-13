ccd76e087e985b7042f7f660df8be3196583c460
vaibhavsethia <vaibhavsethia1998@gmail.com>
2023-06-14 02:16:10 +0530

    fix

--- src/Components/Quotemaster/Receipt.js
+++ src/Components/Quotemaster/Receipt.js
@@ -66,7 +66,7 @@ export class Receipt extends React.PureComponent {
           <Col lg={2}></Col>
         </Row>
         <Row >
-            sdas
+            
         </Row>
       </div>
     )
