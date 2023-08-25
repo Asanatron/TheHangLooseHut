cd58cdbd9da740b5634d687a9502f886cfba9c6c
Vivek Sharma <vivsharp@amazon.com>
2023-08-26 03:06:21 +0530

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/TheHangLooseHut/precache-manifest.0872eb0a0d02e7275163e1ac502fc7a3.js"
+  "/TheHangLooseHut/precache-manifest.347e31cdb60dacaf84269f763ae638fa.js"
 );
 
 workbox.clientsClaim();
